072109243e45afe19bab9ca3e8c490ee6d5376c9
BinRoot <nick722@gmail.com>
2012-02-12 01:45:29 -0500

    Added edge detection algorithm.

--- src/Engine.java
+++ src/Engine.java
@@ -1,8 +1,9 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 
 import javax.imageio.ImageIO;
 
@@ -53,10 +54,24 @@ public class Engine {
 		}
 	}
 	
+	/**
+	 * Analyze the images using an algorithm chosen below.
+	 *  Each algorithm is a method (ex: deltaComparison, edgeDetection)
+	 *  Use this area to test new algorithms.
+	 * @param if1
+	 * @param if2
+	 */
 	public void analyze(ImageFrame if1, ImageFrame if2) {
 		
+		int ALGORITHM = 1;
 		
-		deltaComparison(if1, if2);
+		switch(ALGORITHM) {
+			case 0: deltaComparison(if1, if2);
+					break;
+			case 1: edgeDetection(if1);
+					break;
+		}
+		
 	
 		
 		File output = new File("img_out.png");
@@ -95,16 +110,75 @@ public class Engine {
 		}
 	}
 	
-	public void edgeDetection(ImageFrame if1, ImageFrame if2) {
-		for (int col = 0; col < if1.getHeight(); col++) {
-			for (int row = 0; row < if1.getWidth(); row++) {
-				int[] d1Array = new int[4];
-				if1.getRar().getPixel(row, col, d1Array);
-				System.out.println("image 1: " + d1Array[0] + " " + d1Array[1]
-						+ " " + d1Array[2] + " " + d1Array[3]);
-
+	/**
+	 * ----- ALGORITHM 2 -----
+	 * Detects edges in an image by comparing neighboring pixels
+	 *  and marking at positions that differ above some threshold value
+	 * @param if1 the ImageFrame to analyze
+	 */
+	public void edgeDetection(ImageFrame if1) {
+		for (int col = 1; col < if1.getHeight()-1; col++) {
+			for (int row = 1; row < if1.getWidth()-1; row++) {
 				
-				if1.getBum().setRGB(row, col, 0x00ff00);
+				int[] dCENT = new int[4];
+				if1.getRar().getPixel(row, col, dCENT);
+				int vCENT = dCENT[0]+dCENT[1]+dCENT[2];
+				
+				int[] dNW = new int[4];
+				if1.getRar().getPixel(row-1, col-1, dNW);
+				int vNW = dNW[0]+dNW[1]+dNW[2];
+				
+				int[] dN = new int[4];
+				if1.getRar().getPixel(row, col-1, dN);
+				int vN = dN[0]+dN[1]+dN[2];
+				
+				int[] dNE = new int[4];
+				if1.getRar().getPixel(row+1, col-1, dNE);
+				int vNE = dNE[0]+dNE[1]+dNE[2];
+				
+				int[] dW = new int[4];
+				if1.getRar().getPixel(row-1, col, dW);
+				int vW = dW[0]+dW[1]+dW[2];
+				
+				int[] dE = new int[4];
+				if1.getRar().getPixel(row+1, col, dE);
+				int vE = dE[0]+dE[1]+dE[2];
+				
+				int[] dSW = new int[4];
+				if1.getRar().getPixel(row-1, col+1, dSW);
+				int vSW = dSW[0]+dSW[1]+dSW[2];
+				
+				int[] dS = new int[4];
+				if1.getRar().getPixel(row, col+1, dS);
+				int vS = dS[0]+dS[1]+dS[2];
+				
+				int[] dSE = new int[4];
+				if1.getRar().getPixel(row+1, col+1, dSE);
+				int vSE = dSE[0]+dSE[1]+dSE[2];
+				
+				ArrayList<Integer> colorVals = new ArrayList<Integer>();
+				
+				colorVals.add(vNW);
+				colorVals.add(vN);
+				colorVals.add(vNE);
+				colorVals.add(vW);
+				colorVals.add(vE);
+				colorVals.add(vSW);
+				colorVals.add(vS);
+				colorVals.add(vSE);
+				
+				Collections.sort(colorVals);
+				
+				int extreme1 = colorVals.get(0);
+				int extreme2 = colorVals.get(colorVals.size()-1);
+				
+				int diff = Math.abs(extreme1-extreme2);
+				
+				System.out.println(diff);
+				
+				if(diff>80) {
+					if1.getBum().setRGB(row, col, 0xff00ff00);
+				}
 				
 			}
 		}
